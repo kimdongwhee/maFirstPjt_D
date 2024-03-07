f8ef23703ad075b893a57b5e3c3a5200837e7af7
DongWheeKim <romangrism@gmail.com>
2024-03-08 00:24:19 +0900

    test 커밋 및 반영

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#streamlit run 파일명.py
+#큰글씨
+st.title("title") 
+st.header("header")
+st.subheader("subheader")
+
+#마크다운
+st.markdown("# 안녕\n## 안녕\n### 안녕\n#### 안녕\n##### 안녕")
